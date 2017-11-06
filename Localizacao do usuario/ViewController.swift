31c6848e5726bf69fb4a5f7c3e4e08fbbe4a856e
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-06 15:56:48 -0200

    Configurações para mostrar atualização do usuário

--- Localizacao do usuario/ViewController.swift
+++ Localizacao do usuario/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet var mapa: MKMapView!
+    var gerenciadorLocal = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //configurar bliblioteca - build phases - link bunary with libraries
+        //adicionar CoreLocation
+        gerenciadorLocal.delegate = self//a propria classe ficara reponsavel pelo objeto gerenciadorLocal
+        gerenciadorLocal.desiredAccuracy = kCLLocationAccuracyBest//configurar precisao
+        gerenciadorLocal.requestWhenInUseAuthorization()//pedir autorizacao para o usuario
+        gerenciadorLocal.startUpdatingLocation()//comeca atualizar a localizacao atual
+        
+        //configuracao adicional o Info.plist para autorizacao do usuario
+        //Em "Required device capabilities" - add "location-services" e "gps"
+        //Add "Privacy - Location When In Use Usage Description" e uma mensagem "Necessário acesso à sua localização para funcionamento do App!"
+        
+        
+        
+        
+        
+        /*let latitude: CLLocationDegrees = -23.586974
+        let longetude: CLLocationDegrees = -46.657355
+        
+        let deltaLatitude: CLLocationDegrees = 0.01
+        let deltaLongetude: CLLocationDegrees = 0.01
+        
+        //configurar coordenadas 2D
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longetude)
+        
+        //configurar o zoom
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongetude)
+        
+        //configurar uma regiao para o mapa
+        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)
+        
+        //exibir regiao no mapa
+        mapa.setRegion(regiao, animated: true)
+        
+        //configurar anotacao
+        let anotacao = MKPointAnnotation()
+        
+        anotacao.coordinate = localizacao
+        anotacao.title = "Parque do Ibirapuera"
+        anotacao.subtitle = "O Parque Ibirapuera é o mais importante parque urbano da cidade de São Paulo, no Brasil."
+        
+        mapa.addAnnotation(anotacao)*/
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
